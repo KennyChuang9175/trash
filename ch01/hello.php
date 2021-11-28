cdde4127b1eec5a959f4a2582d4ca3e4d9b203ff
s07352011 <S07352011@thu.edu.tw>
2021-11-28 18:10:46 +0800

    123

---
+++ ch01/hello.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>我的第一個 PHP 程式</title>
+</head>
+<body>
+<?php
+echo("Hello World!");
+phpinfo();
+?>
+</body>
+</html>
---
+++ test site/scripts/main.js
@@ -0,0 +1,38 @@
+﻿var myHeading = document.querySelector('h1');
+myHeading.textContent = "Hello pro dog!"
+setUserName();
+document.querySelector('h1').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/dog1.jpg') {
+      myImage.setAttribute ('src','images/dog2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/dog1.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('你喜歡小吉嗎? love/hate');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'I ' + myName+' this pro dog';
+  }
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'I ' + storedName+ ' this pro dog' ;
+}
+myButton.onclick = function() {
+  setUserName();
+}
